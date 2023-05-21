0fd8f2dc6de3431923978f974bcffa3682f012d4
lakkundilata <lakkundilata@gmail.com>
2023-05-21 18:17:20 +0530

    21-5-2023

---
+++ class.py
@@ -0,0 +1,17 @@
+class students:
+    studentName="xyz"
+    studentRolnum=38
+    marks1=50
+    marks2=78
+    marks3=80
+    def studentInformation(self,studentName,studentRolnum,marks1,marks2,marks3):
+        print("abc",self.studentName,self.studentRolnum,self.marks1,self.marks2,self.marks3)
+    def __init__(self,studentName,studentRolnum,marks1,marks2,marks3):
+        self.studentName="laxmi"
+        self.studentRolnum=32
+        self.marks1=70
+        self.marks2=90
+        self.marks3=30
+        sum=marks1+marks2+marks3
+        print(sum)
+
